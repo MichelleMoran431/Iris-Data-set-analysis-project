299114a71531553394308890a7c4b6fd3b7ced84
unknown <g00387856@gmit.ie>
2020-04-13 10:57:32 +0100

    using describe()

--- analysis.py
+++ analysis.py
@@ -3,6 +3,16 @@ import csv
 
 
 import pandas
-df = pandas.read_csv( "C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt")
-print(df)
+#import numpy as np
+#df = pandas.read_csv( "C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt", index_col=0 )
+#type (df)
+#desc = df ["petal_width" ].describe()
+#desc
+#print(desc)
 
+df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt", sep=',', names=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
+desc = df.describe('index_col=0')
+print(desc)
+
+#type (df)
+#print (df)
