ae5e071b5ab48086cf593b636b017e631cc7e768
unknown <g00387856@gmit.ie>
2020-04-14 15:21:30 +0100

    assigned column headings and summary

--- analysis.py
+++ analysis.py
@@ -10,9 +10,14 @@ import pandas
 #desc
 #print(desc)
 
-df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt", sep=',', names=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
-desc = df.describe('index_col=0')
+#df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt", sep=',', names=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
+df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt")
+attributes = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
+df.columns = attributes
+
+desc = df.describe()
 print(desc)
+print(df.head())
 
 #type (df)
 #print (df)
