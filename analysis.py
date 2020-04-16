185affdec737b1eeb63a99bb36cd4a1cb4392d9b
unknown <g00387856@gmit.ie>
2020-04-16 09:21:52 +0100

    output summary to text file

--- analysis.py
+++ analysis.py
@@ -3,21 +3,12 @@ import csv
 
 
 import pandas
-#import numpy as np
-#df = pandas.read_csv( "C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt", index_col=0 )
-#type (df)
-#desc = df ["petal_width" ].describe()
-#desc
-#print(desc)
 
-#df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt", sep=',', names=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
 df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt")
 attributes = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
 df.columns = attributes
 
 desc = df.describe()
-print(desc)
-print(df.head())
+print(df.head() ,file = open("Summary File", "a"))
+print(desc ,file = open("Summary File", "a"))
 
-#type (df)
-#print (df)
