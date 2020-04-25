4318d8c5a9f6f2b2c5c9fb99db20d834e7e989f2
unknown <g00387856@gmit.ie>
2020-04-25 11:09:50 +0100

    update png.files'

--- analysis.py
+++ analysis.py
@@ -9,19 +9,25 @@ import numpy
 
 #Use pandas to load the data and examine the data through descriptive statistics and data visualization.
 import pandas
+import numpy
+
 
 df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt")
 attributes = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"] 
 df.columns = attributes
 
 # Examining the content of datasets through statistics , info and first 5 data lines
-desc = df.describe()
+perc =[.20,.40,.60,.80]
+include = ['object','float','int']
+desc = df.describe(percentiles = perc, include = include)
 head  = df.head ()
 info  = df.info
-#print(df.describe())
+
+
+print(df.describe(percentiles = perc, include = include))
 
 # output the results to a text file
-df = open("SummaryA.txt", "w")
+df = open("Summary.txt", "w")
 print(desc,file = df)
 print(head,file = df)
 print(info,file = df)
@@ -29,12 +35,10 @@ print(info,file = df)
 
 
 
-
-# using for plotting /visualising data in python
-
 #References : https://realpython.com/python-histograms/#building-up-from-the-base-histogram-calculations-in-numpy
 #http://www.datasciencemadesimple.com/histogram-in-python-using-matplotlib/
 
+# Plotting /visualising data in python
 import matplotlib.pyplot as plt
 df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt")
 #Adding column headings to the dataframes via attributes
@@ -45,21 +49,24 @@ df.columns = attributes
 # Version no. 1 Histograms for each variable of the Iris data set using following modules
 
 #References : https://www.machinelearningplus.com/plots/matplotlib-histogram-python-examples/
+
 import matplotlib.pyplot as plt
 import pandas
 import numpy as np
 from matplotlib.patches import Rectangle 
 
 df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt")
+# using attributes to name the columns in the dataframe 
 attributes = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
 df.columns = attributes
 
 
 plt.title ("Sepal Length in cm")
+# Associating species type with attribute in dataframe
 x1= df.loc[df.species == 'setosa','sepal_length']
 x2= df.loc[df.species == 'versicolor','sepal_length']
 x3= df.loc[df.species == 'virginica', 'sepal_length']
-kwargs = dict(alpha=0.5, bins=20)
+kwargs = dict(alpha=0.5, bins=20) #**kwargs is a dictionary of keyword arguments.
 plt.hist(x1, **kwargs, color ='b', label = 'setosa',edgecolor='black')
 plt.hist(x2, **kwargs,color ='r', label = 'versicolor',edgecolor='black')
 plt.hist(x3, **kwargs,color ='g', label = 'virginica',edgecolor='black')
@@ -107,7 +114,7 @@ plt.title ("petal width")
 x1= df.loc[df.species == 'setosa','petal_width']
 x2= df.loc[df.species == 'versicolor','petal_width']
 x3= df.loc[df.species == 'virginica', 'petal_width']
-kwargs = dict(alpha=0.5, bins=20)
+kwargs = dict(alpha=0.5, bins=20) 
 plt.hist(x1, **kwargs, color ='b', label = 'setosa',edgecolor='black')
 plt.hist(x2, **kwargs,color ='r', label = 'versicolor',edgecolor='black')
 plt.hist(x3, **kwargs,color ='g', label = 'virginica',edgecolor='black')
@@ -119,7 +126,10 @@ plt.savefig ("petal_width version 1.png")
 plt.clf()
 
 
+#Reference:https://www.machinelearningplus.com/plots/matplotlib-histogram-python-examples/
+
 # Version 2 : Histograms created using Seaborn module to better represent the Iris data set 
+
 import seaborn as sns
 sns.FacetGrid(df,hue="species").map(sns.distplot,'petal_length').add_legend()
 
@@ -132,6 +142,7 @@ sns.FacetGrid(df,hue="species").map(sns.distplot,'sepal_length').add_legend()
 
 #Using Seaborn module to give a scatterplot representation of the variables in pairs using pairplot
 #References : http://www.datasciencemadesimple.com/histogram-in-python-using-matplotlib/
+
 import seaborn as sns
 sns.set(style="ticks")
 df = pandas.read_csv("C:\\Users\\User\\Desktop\\Iris-Data-set-analysis-project\\Irisdataset.txt")
